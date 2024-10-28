8c9d96d4273c03765d84bd937738b41eb0fd0790
jaemin <jjm7655@gmail.com>
2024-10-28 21:20:28 +0900

    refactor:banking service Swagger package 수정

--- banking-service/src/main/java/com/fastcampus/banking/SwaggerConfig.java
+++ banking-service/src/main/java/com/fastcampus/banking/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 			.apiInfo(apiInfo())
 			.select()
-			.apis(RequestHandlerSelectors.basePackage("com.fastcampuspay.banking"))
+			.apis(RequestHandlerSelectors.basePackage("com.fastcampus.banking"))
 			.paths(PathSelectors.any())
 			.build();
 	}
