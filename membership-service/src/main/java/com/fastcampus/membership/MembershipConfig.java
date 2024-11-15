0e28dbe1e909db21f5654a4bd53921854ddf4429
jaemin <jjm7655@gmail.com>
2024-11-15 21:28:02 +0900

    feat: kafka 세팅 및 consumer 설정

---
+++ membership-service/src/main/java/com/fastcampus/membership/MembershipConfig.java
@@ -0,0 +1,9 @@
+package com.fastcampus.membership;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fastcampus.common")
+public class MembershipConfig {
+}
